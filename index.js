c46c09847d4423c92a00565f0ade2bbc79a07649
afigard <afigard@pop-os.localdomain>
2021-05-20 10:31:07 +0200

    API_tele

--- index.js
+++ index.js
@@ -1,97 +1,51 @@
-const express = require('express');
-const app = express();
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var xhub = require("express-x-hub");
 
-app.use(express.json());
+//Const
+var xhubSecret = "MySecretKey";
+var port = "8085";
+var host = "localhost";
 
-//identification
-const Pool = require('pg').Pool
-const pool = new Pool({
+//Secret key
+app.use(xhub({ algorithm: "sha1", secret: xhubSecret }));
 
-  user: '',
-  host: '',
-  database: '',
-  password: '',
-  port: '',
+// Configure express json
+app.use(bodyParser.json());
 
-})
+// Main : Start the express http server
+var server = app.listen(port, host, function() {
+  console.log(
+    "App listening at http://%s:%s",
+    server.address().address,
+    server.address().port,
+  );
+});
 
-//connexion & erreur
-pool.connect((err, client, release) => {
-  if (err) {
-    return console.error('Error acquiring client', err.stack)
-  }
-  client.query('SELECT NOW()', (err, result) => {
-    release()
-    if (err) {
-      return console.error('Error executing query', err.stack)
-    }
-    console.log(result.rows)
-  })
-})
-
-  
-app.get('/:id_equipement', (req,res) => {
-try {
-  pool.query('select * from rhino.elements where id_equipement=$1', [req.params.id_equipement], (error, results) => {
-    if (error) {
-      console.log(error)
-      res.status(500).json(null)
-    }
-    if (results===undefined) res.status(404)
-    if (results !== undefined){
-      res.status(200).json(results.rows)
-    } else res.status(404)
-  })
-  } catch(e){
-    res.status(500)
+// Add default route
+app.post("/webhook", function(req, res) {
+  if (!req.isXHubValid()) {
+    res.status(400).send("Invalid X-Hub Request");
+    console.log("Secret key is invalid");
+    return;
   }
 
-});
+  switch (req.headers["x-github-event"]) {
+    //Event create (Branch, or tag created)
+    case "create":
+      res.send("Event create trigger");
+      console.log("Create event");
+      break;
 
-app.get('/liste_types/:id_types', (req,res) => {
-try {
-  pool.query('select libelle from v1.libelles where id=$1', [req.params.id_types] , (error, results) => {
-    if (error) {
-      console.log(error)
-      res.status(500).json(null)
-    }
-    if (results===undefined) res.status(404)
-    if (results !== undefined){
-      res.status(200).json(results.rows)
-    } else res.status(404)
-  })
-  } catch(e){
-    res.status(500)
+    //Event release (Release published in a repository)
+    case "release":
+      res.send("Event release trigger");
+      console.log("Release Event");
+      break;
+
+    default:
+      res.status(400).send("Event not supported : " + command);
+      console.log("Event not supported : " + req.headers["X-Github-Event"]);
   }
-
 });
-
-
-app.post('/:id_equipement', (req,res) => {
-  const { id_equipement , id_element , libelle } = req.body
-	console.log(req.body)
-  pool.query('INSERT INTO rhino.elements (id_equipement ,id_element ,libelle) VALUES ($1, $2, $3)', [id_equipement,id_element,libelle], (error, results) => {
-    if (error) {
-      throw error
-      console.log(error)
-      res.status(500).json()
-    }
-    console.log(results)
-    res.status(200).json(results.rows)
-  })
-});
-
-app.delete('/:id_equipement/:id_element', (req,res) => {
-     const id_element = parseInt(req.params.id_element)
-     const id_equipement = parseInt(req.params.id_equipement)
-    pool.query('DELETE FROM rhino.elements WHERE id_equipement = $1 AND id_element = $2 LIMIT 1', [id_equipement, id_element], (error, results) => {
-    if (error) {
-      throw error
-    }
-    res.status(200).send(`Element supprime numero : ${id_element}`)
-   })
-});
-
-app.listen(1234, () => {
-    console.log("Serveur : OK")
-  })
